c32981d681916a977741ea247912fe2595954da0
ZZF-4 <3115767601@qq.com>
2024-10-14 17:15:00 +0800

    用里程计计算小车的额移动距离

--- xiaoche_ws/src/zuoye/src/odom_distance_node.cpp
+++ xiaoche_ws/src/zuoye/src/odom_distance_node.cpp
@@ -2,6 +2,8 @@
 #include <nav_msgs/Odometry.h>
 #include <math.h>
 float distance = 0.0;
+//订阅彩色图像和深度图像的订阅者对象
+//分别用于发布处理后的彩色图像和深度图像的发布者对象
 void doMsg(const nav_msgs::Odometry::ConstPtr& msg)
 {
     float speed_x=0;    
@@ -14,6 +16,7 @@ void doMsg(const nav_msgs::Odometry::ConstPtr& msg)
     distance += 0.1 * speed_x;
     ROS_INFO("里程计的距离为：%.6f",distance );
 }
+//主函数
 int main(int argc, char  *argv[])
 {
     setlocale(LC_ALL,"");
